7ebaf902139e7cdfbd78bfa4108a87a37ac3d19b
dsemkin <semkin_dmitriy10@vk.com>
2025-08-02 17:32:13 +0400

    Улучшил UiButton

--- core/design/src/main/java/com/andef/dailyquiz/core/design/button/type/UiButtonType.kt
+++ core/design/src/main/java/com/andef/dailyquiz/core/design/button/type/UiButtonType.kt
@@ -1,6 +1,7 @@
 package com.andef.dailyquiz.core.design.button.type
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import com.andef.dailyquiz.core.design.DeepPurple
 import com.andef.dailyquiz.core.design.IndigoBlue
 import com.andef.dailyquiz.core.design.White
@@ -8,5 +9,11 @@ import com.andef.dailyquiz.core.design.White
 sealed class UiButtonType(val backgroundColor: Color, val contentColor: Color) {
     data object Main : UiButtonType(backgroundColor = IndigoBlue, contentColor = White)
     data object Secondary : UiButtonType(backgroundColor = White, contentColor = DeepPurple)
-    data object History : UiButtonType(backgroundColor = White, contentColor = IndigoBlue)
+    data class WithIcon(
+        val icon: Painter,
+        val contentDescription: String,
+    ) : UiButtonType(
+        backgroundColor = White,
+        contentColor = IndigoBlue
+    )
 }
---
+++ feature/start/presentation/src/main/java/com/andef/dailyquiz/start/presentation/StartScreen.kt
@@ -0,0 +1,10 @@
+package com.andef.dailyquiz.start.presentation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun StartScreen(navHostController: NavHostController, paddingValues: PaddingValues) {
+
+}
