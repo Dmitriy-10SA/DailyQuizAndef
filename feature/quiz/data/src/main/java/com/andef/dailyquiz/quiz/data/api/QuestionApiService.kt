a30025398c05bf84c9a31759f7c298370a358eaa
dsemkin <semkin_dmitriy10@vk.com>
2025-08-02 18:48:50 +0400

    data для quiz фичи

---
+++ feature/quiz/data/src/main/java/com/andef/dailyquiz/quiz/data/api/QuestionApiService.kt
@@ -0,0 +1,14 @@
+package com.andef.dailyquiz.quiz.data.api
+
+import com.andef.dailyquiz.quiz.data.dto.QuestionApiResponseDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface QuestionApiService {
+    @GET("api.php")
+    suspend fun loadQuestions(
+        @Query("amount") amount: Int = 5,
+        @Query("category") category: Int,
+        @Query("difficulty") difficulty: String
+    ): QuestionApiResponseDto
+}
