8f89d9ea424f2af04588b42833420798bdf8ecaf
dsemkin <semkin_dmitriy10@vk.com>
2025-08-03 18:33:57 +0400

    Поправил DailyQuizApp

--- app/src/main/java/com/andef/dailyquiz/DailyQuizApp.kt
+++ app/src/main/java/com/andef/dailyquiz/DailyQuizApp.kt
@@ -4,5 +4,7 @@ import android.app.Application
 import com.andef.dailyquiz.core.di.common.DaggerAppComponent
 
 class DailyQuizApp : Application() {
-    val component by lazy { DaggerAppComponent.factory().create(this) }
+    val component by lazy {
+        DaggerAppComponent.factory().create(this)
+    }
 }
