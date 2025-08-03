94cf5fe4257089698b4cc696da7afdca49c70387
dsemkin <semkin_dmitriy10@vk.com>
2025-08-03 19:06:03 +0400

    Поправил всю папку network в core:data

--- core/data/src/main/java/com/andef/dailyquiz/core/data/network/ApiFactory.kt
+++ core/data/src/main/java/com/andef/dailyquiz/core/data/network/ApiFactory.kt
@@ -4,6 +4,11 @@ import com.andef.dailyquiz.quiz.data.api.QuestionApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+/**
+ * Фабрика для получения QuestionApiService
+ *
+ * @property QuestionApiService ApiService для получения вопросов к викторине
+ */
 class ApiFactory {
     companion object {
         private const val BASE_URL = "https://opentdb.com/"
