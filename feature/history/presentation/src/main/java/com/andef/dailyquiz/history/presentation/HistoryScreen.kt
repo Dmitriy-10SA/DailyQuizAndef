3cd2a8af0215de037a4dd3572df914c2aeef7d85
dsemkin <semkin_dmitriy10@vk.com>
2025-08-03 16:17:20 +0400

    HistoryScreen

--- feature/history/presentation/src/main/java/com/andef/dailyquiz/history/presentation/HistoryScreen.kt
+++ feature/history/presentation/src/main/java/com/andef/dailyquiz/history/presentation/HistoryScreen.kt
@@ -3,19 +3,21 @@ package com.andef.dailyquiz.history.presentation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,105 +50,123 @@ fun HistoryScreen(
     val viewModel: HistoryScreenViewModel = viewModel(factory = viewModelFactory)
     val state = viewModel.state.collectAsState()
 
-    when (state.value.isLoading) {
-        true -> UiLoading()
-        false -> {
-            Column(
-                modifier = Modifier
-                    .padding(top = paddingValues.calculateTopPadding())
-                    .navigationBarsPadding()
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .padding(top = 38.dp)
-                        .padding(bottom = 40.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    UiIconButton(
-                        modifier = Modifier.size(36.dp),
-                        icon = painterResource(com.andef.dailyquiz.core.design.R.drawable.arrow_back),
-                        contentDescription = stringResource(com.andef.dailyquiz.core.design.R.string.back),
-                        onClick = navHostController::popBackStack
-                    )
-                    Text(
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(end = 36.dp),
-                        text = stringResource(com.andef.dailyquiz.core.design.R.string.history),
-                        fontWeight = FontWeight.W900,
-                        color = White,
-                        fontFamily = FontFamily(Font(com.andef.dailyquiz.core.design.R.font.inter)),
-                        letterSpacing = 0.sp,
-                        lineHeight = 32.sp,
-                        fontSize = 32.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-                if (state.value.quizzes.isEmpty()) {
-                    EmptyQuizzesContent(onButtonClick = navHostController::popBackStack)
-                } else {
-                    LazyColumn(
-                        modifier = Modifier.weight(1f),
-                        verticalArrangement = Arrangement.spacedBy(16.dp)
-                    ) {
-                        items(items = state.value.quizzes, key = { it.id }) { quiz ->
-                            HistoryItemCard(
-                                quizId = quiz.id,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 20.dp),
-                                correctAnsCnt = quiz.correctAnswersCnt,
-                                date = quiz.date,
-                                time = quiz.time
-                            )
-                        }
-                    }
-                }
-            }
-        }
+    when {
+        state.value.isLoading -> UiLoading()
+        state.value.quizzes.isEmpty() -> EmptyQuizzesContent(
+            paddingValues = paddingValues,
+            onButtonClick = navHostController::popBackStack,
+            navHostController = navHostController
+        )
+
+        else -> MainContent(navHostController = navHostController, state = state)
     }
 }
 
 @Composable
-private fun ColumnScope.EmptyQuizzesContent(onButtonClick: () -> Unit) {
-    UiCard(
+private fun MainContent(navHostController: NavHostController, state: State<HistoryScreenState>) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item { Spacer(modifier = Modifier.statusBarsPadding()) }
+        item { Header(navHostController = navHostController) }
+        items(items = state.value.quizzes, key = { it.id }) { quiz ->
+            Spacer(modifier = Modifier.padding(16.dp))
+            HistoryItemCard(
+                quizId = quiz.id,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                correctAnsCnt = quiz.correctAnswersCnt,
+                date = quiz.date,
+                time = quiz.time
+            )
+        }
+        item { Spacer(modifier = Modifier.padding(16.dp)) }
+        item { Spacer(modifier = Modifier.navigationBarsPadding()) }
+    }
+}
+
+@Composable
+private fun Header(navHostController: NavHostController) {
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 20.dp)
+            .padding(top = 38.dp)
+            .padding(bottom = 24.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(
-            modifier = Modifier.padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(40.dp)
-        ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = stringResource(com.andef.dailyquiz.core.design.R.string.empty_history_card_text),
-                fontWeight = FontWeight.W400,
-                color = Black,
-                fontFamily = FontFamily(Font(com.andef.dailyquiz.core.design.R.font.inter)),
-                letterSpacing = 0.sp,
-                lineHeight = 20.sp,
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center
-            )
-            UiButton(
-                onClick = onButtonClick,
-                modifier = Modifier.fillMaxWidth(),
-                text = stringResource(com.andef.dailyquiz.core.design.R.string.start_quiz)
-            )
-        }
+        UiIconButton(
+            modifier = Modifier.size(36.dp),
+            icon = painterResource(com.andef.dailyquiz.core.design.R.drawable.arrow_back),
+            contentDescription = stringResource(com.andef.dailyquiz.core.design.R.string.back),
+            onClick = navHostController::popBackStack
+        )
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 36.dp),
+            text = stringResource(com.andef.dailyquiz.core.design.R.string.history),
+            fontWeight = FontWeight.W900,
+            color = White,
+            fontFamily = FontFamily(Font(com.andef.dailyquiz.core.design.R.font.inter)),
+            letterSpacing = 0.sp,
+            lineHeight = 32.sp,
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center
+        )
     }
-    Spacer(modifier = Modifier.weight(1f))
-    Image(
+}
+
+@Composable
+private fun EmptyQuizzesContent(
+    navHostController: NavHostController,
+    paddingValues: PaddingValues,
+    onButtonClick: () -> Unit
+) {
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 64.dp)
-            .padding(horizontal = 90.dp),
-        contentScale = ContentScale.FillWidth,
-        painter = painterResource(com.andef.dailyquiz.core.design.R.drawable.logo),
-        contentDescription = stringResource(com.andef.dailyquiz.core.design.R.string.daily_quiz_logo)
-    )
+            .fillMaxSize()
+            .padding(top = paddingValues.calculateTopPadding())
+            .navigationBarsPadding()
+    ) {
+        Header(navHostController = navHostController)
+        UiCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+                .padding(horizontal = 20.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(40.dp)
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(com.andef.dailyquiz.core.design.R.string.empty_history_card_text),
+                    fontWeight = FontWeight.W400,
+                    color = Black,
+                    fontFamily = FontFamily(Font(com.andef.dailyquiz.core.design.R.font.inter)),
+                    letterSpacing = 0.sp,
+                    lineHeight = 20.sp,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
+                )
+                UiButton(
+                    onClick = onButtonClick,
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(com.andef.dailyquiz.core.design.R.string.start_quiz)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 65.dp)
+                .padding(horizontal = 90.dp),
+            contentScale = ContentScale.FillWidth,
+            painter = painterResource(com.andef.dailyquiz.core.design.R.drawable.logo),
+            contentDescription = stringResource(com.andef.dailyquiz.core.design.R.string.daily_quiz_logo)
+        )
+    }
 }
